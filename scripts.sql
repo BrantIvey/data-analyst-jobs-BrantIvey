c2abcdfb4476060e65f633ff8d2c48d3a466e81b
BrantIvey <bivey10@gmail.com>
2020-01-18 14:24:11 -0600

    Question 3 complete

--- scripts.sql
+++ scripts.sql
@@ -14,3 +14,18 @@ Answer: XTO Land Data Analyst
 SELECT *
 FROM data_analyst_jobs
 LIMIT 10;
+
+
+/* 3. How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
+
+Answer: TN - 21; KY/TN - 27
+
+*/
+
+SELECT COUNT (*)
+FROM data_analyst_jobs
+WHERE location = 'TN';
+
+SELECT COUNT (*)
+FROM data_analyst_jobs
+WHERE location = 'TN' OR location = 'KY';
