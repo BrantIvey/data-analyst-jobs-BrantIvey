718c6fcd380346fec476302e054cd37a2291cf0b
BrantIvey <bivey10@gmail.com>
2020-01-18 13:52:34 -0600

    Question 2 complete

--- scripts.sql
+++ scripts.sql
@@ -4,3 +4,13 @@
 
 SELECT COUNT (*)
 FROM data_analyst_jobs;
+
+/* 2.	Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row? 
+
+Answer: XTO Land Data Analyst
+
+*/
+
+SELECT *
+FROM data_analyst_jobs
+LIMIT 10;
